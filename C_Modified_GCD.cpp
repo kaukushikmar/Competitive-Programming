b8f252fe885d6b0de85768abdeb3846dd65f881d
kaushik kumar <kaushik@flagright.com>
2022-08-04 07:17:25 +0530

    modified gcd

--- C_Modified_GCD.cpp
+++ C_Modified_GCD.cpp
@@ -19,4 +19,31 @@ int main()
     {
         cin >> low[i] >> high[i];
     }
+
+    int d = gcd(a, b);
+    vector<int> v;
+    for (int i = 1; i * i <= d; i++)
+    {
+        if (d % i == 0)
+        {
+            v.push_back(i);
+            if (d / i > i)
+                v.push_back(d / i);
+        }
+    }
+
+    sort(v.begin(), v.end());
+    for (int i = 0; i < n; i++)
+    {
+        int ans = -1;
+        for (int j = v.size() - 1; j >= 0; j--)
+        {
+            if (low[i] <= v[j] && v[j] <= high[i])
+            {
+                ans = v[j];
+                break;
+            }
+        }
+        cout << ans << endl;
+    }
 }
